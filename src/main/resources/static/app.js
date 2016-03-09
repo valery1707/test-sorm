508e98dc170e7df89cd792096fdbeca324b57b29
Valeriy.Vyrva <valery1707@gmail.com>
2016-03-09 17:18:29 +0600

    Отображение данных с сервера. Без пагинации и фильтров

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,8 +1,43 @@
 'use strict';
 
+var apiBaseUrl = 'http://localhost:8080';
+
 // Declare app level module which depends on views, and components
 angular.module('app', [
 	'ngRoute',
 	'ngResource'
-])
+	, 'smart-table'
+]).
+
+factory('dataService', ['$resource', function ($resource) {
+	return $resource(apiBaseUrl + '/data', {}, {
+		query: {
+			method: 'GET',
+			params: {},
+			isArray: false,
+			cache: false
+		}
+	});
+}]).
+controller('dataCtrl', ['$scope', 'dataService', function ($scope, service) {
+	$scope.displayed = [];
+	$scope.callServer = function (tableState) {
+		$scope.isLoading = true;
+
+		//console.log(tableState);
+
+		var pagination = tableState.pagination;
+
+		var start = pagination.start || 0;     // This is NOT the page number, but the index of item in the list that you want to use to display the table.
+		var number = pagination.number || 10;  // Number of entries showed per page.
+
+		service.query(function (data) {
+			//console.log(data);
+			$scope.displayed = data.content;
+			//set the number of pages so the pagination can update
+			tableState.pagination.numberOfPages = data.totalPages;
+			$scope.isLoading = false;
+		});
+	};
+}])
 ;
