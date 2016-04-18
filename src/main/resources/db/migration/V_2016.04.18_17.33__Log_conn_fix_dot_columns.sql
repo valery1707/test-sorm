47b7feaf2b5e0d6e01a17c83ecea7fa5ddb435f3
Valeriy.Vyrva <valery1707@gmail.com>
2016-04-18 19:15:03 +0600

    SQL: одновременная поддержка MySQL и H2

--- src/main/resources/db/migration/V_2016.04.18_17.33__Log_conn_fix_dot_columns.sql
+++ src/main/resources/db/migration/V_2016.04.18_17.33__Log_conn_fix_dot_columns.sql
@@ -1,5 +1,16 @@
-ALTER TABLE conn
-CHANGE COLUMN `id.orig_h` id_orig_h VARCHAR(45),
-CHANGE COLUMN `id.orig_p` id_orig_p INT,
-CHANGE COLUMN `id.resp_h` id_resp_h VARCHAR(45),
-CHANGE COLUMN `id.resp_p` id_resp_p INT;
+ALTER TABLE conn ADD COLUMN id_orig_h VARCHAR(45) AFTER `id.orig_h`;
+ALTER TABLE conn ADD COLUMN id_orig_p INT AFTER `id.orig_p`;
+ALTER TABLE conn ADD COLUMN id_resp_h VARCHAR(45) AFTER `id.resp_h`;
+ALTER TABLE conn ADD COLUMN id_resp_p INT AFTER `id.resp_p`;
+
+UPDATE conn
+SET
+		id_orig_h = `id.orig_h`,
+		id_orig_p = `id.orig_p`,
+		id_resp_h = `id.resp_h`,
+		id_resp_p = `id.resp_p`;
+
+ALTER TABLE conn DROP COLUMN `id.orig_h`;
+ALTER TABLE conn DROP COLUMN `id.orig_p`;
+ALTER TABLE conn DROP COLUMN `id.resp_h`;
+ALTER TABLE conn DROP COLUMN `id.resp_p`;
