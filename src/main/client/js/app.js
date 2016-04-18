d73c655cd0465a6cd24e85a6e1a9b128148749b9
Valeriy.Vyrva <valery1707@gmail.com>
2016-04-18 19:03:26 +0600

    Переиспользование общего клиентского кода

--- src/main/client/js/app.js
+++ src/main/client/js/app.js
@@ -1,6 +1,19 @@
 'use strict';
 
-var apiBaseUrl = '/api';
+const apiBaseUrl = '/api';
+const serviceCommonConfig = {
+	query: {
+		method: 'GET',
+		params: {},
+		isArray: false,
+		cache: false
+	},
+	filter: {
+		method: 'POST',
+		isArray: false,
+		cache: false
+	}
+};
 
 // Declare app level module which depends on views, and components
 angular.module('app', [
@@ -39,22 +52,91 @@ directive('dateTimePicker', function(){
 	}
 }).
 
-factory('connService', ['$resource', function ($resource) {
-	return $resource(apiBaseUrl + '/conn', {}, {
-		query: {
-			method: 'GET',
-			params: {},
-			isArray: false,
-			cache: false
-		},
-		filter: {
-			method: 'POST',
-			isArray: false,
-			cache: false
-		}
-	});
+service('gridHelper', [function () {
+	return function ($scope, service, paginationOptions, gridExt) {
+		$scope.loadPage = function () {
+			$scope.isLoading = true;
+
+			var pageable = {
+				size: paginationOptions.pageSize,
+				page: (paginationOptions.pageNumber - 1),
+				sort: paginationOptions.sort
+			};
+
+			service.filter(pageable, $scope.filterModel, function (data) {
+				//console.log(data);
+				$scope.gridOptions.totalItems = data.totalElements;
+				$scope.gridOptions.data = data.content;
+				$scope.isLoading = false;
+			});
+		};
+		$scope.gridOptions = {
+			paginationPageSizes: [25, 50, 75],
+			paginationPageSize: paginationOptions.pageSize,
+			useExternalPagination: true,
+			useExternalSorting: true,
+			enableFiltering: true,
+			useExternalFiltering: true,
+			onRegisterApi: function(gridApi) {
+				$scope.gridApi = gridApi;
+				$scope.gridApi.core.on.sortChanged($scope, function(grid, sortColumns) {
+					if (sortColumns.length == 0) {
+						paginationOptions.sort = null;
+					} else {
+						paginationOptions.sort = [];
+						for (var i = 0; i < sortColumns.length; i++) {
+							var col = sortColumns[i];
+							paginationOptions.sort.push(col.field + ',' + col.sort.direction.toUpperCase())
+						}
+					}
+					$scope.loadPage();
+				});
+				gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
+					paginationOptions.pageNumber = newPage;
+					paginationOptions.pageSize = pageSize;
+					$scope.loadPage();
+				});
+				$scope.gridApi.core.on.filterChanged($scope, function () {
+					var grid = this.grid;
+					for (var i = 0; i < grid.columns.length; i++) {
+						var col = grid.columns[i];
+						var mapper = col.colDef.filterTermMapper;
+						var getter = function (filter, mapper) {
+							var term = filter.term;
+							var type = typeof term;
+							if (type != 'boolean' && type != 'number') {
+								if (!term) {
+									return undefined;
+								}
+							}
+							mapper = filter.filterTermMapper || mapper;
+							if (mapper) {
+								term = mapper(term);
+							}
+							return term;
+						};
+						if (col.filters.length == 1) {
+							$scope.filterModel[col.field] = getter(col.filters[0], mapper);
+						} else {
+							$scope.filterModel[col.field] = [];
+							for (var t = 0; t < col.filters.length; t++) {
+								$scope.filterModel[col.field].push(getter(col.filters[t], mapper))
+							}
+						}
+					}
+					$scope.loadPage();
+				});
+			}
+		};
+		$scope.gridOptions = jQuery.merge($scope.gridOptions, gridExt);
+		return $scope.gridOptions;
+	}
 }]).
-controller('connCtrl', ['$scope', 'connService', 'uiGridConstants', function ($scope, service, uiGridConstants) {
+
+factory('connService', ['$resource', function ($resource) {
+	return $resource(apiBaseUrl + '/conn', {}, serviceCommonConfig);
+}]).
+controller('connCtrl', ['$scope', 'connService', 'uiGridConstants', 'gridHelper', function ($scope, service, uiGridConstants, gridHelper) {
 	$scope.filterModel = {};
 
 	var paginationOptions = {
@@ -63,13 +145,7 @@ controller('connCtrl', ['$scope', 'connService', 'uiGridConstants', function ($s
 		sort: ["ts,ASC"]
 	};
 
-	$scope.gridOptions = {
-		paginationPageSizes: [25, 50, 75],
-		paginationPageSize: 25,
-		useExternalPagination: true,
-		useExternalSorting: true,
-		enableFiltering: true,
-		useExternalFiltering: true,
+	gridHelper($scope, service, paginationOptions, {
 		columnDefs: [
 			{
 				field: 'ts',
@@ -111,75 +187,8 @@ controller('connCtrl', ['$scope', 'connService', 'uiGridConstants', function ($s
 			{field: 'idRespHost', filter: {placeholder: 'IP/CIDR'}},
 			{field: 'idRespPort', filter: {placeholder: '<, <=, =, >, >=, ... '}},
 		],
-		onRegisterApi: function(gridApi) {
-			$scope.gridApi = gridApi;
-			$scope.gridApi.core.on.sortChanged($scope, function(grid, sortColumns) {
-				if (sortColumns.length == 0) {
-					paginationOptions.sort = null;
-				} else {
-					paginationOptions.sort = [];
-					for (var i = 0; i < sortColumns.length; i++) {
-						var col = sortColumns[i];
-						paginationOptions.sort.push(col.field + ',' + col.sort.direction.toUpperCase())
-					}
-				}
-				getPage();
-			});
-			gridApi.pagination.on.paginationChanged($scope, function (newPage, pageSize) {
-				paginationOptions.pageNumber = newPage;
-				paginationOptions.pageSize = pageSize;
-				getPage();
-			});
-			$scope.gridApi.core.on.filterChanged($scope, function () {
-				var grid = this.grid;
-				for (var i = 0; i < grid.columns.length; i++) {
-					var col = grid.columns[i];
-					var mapper = col.colDef.filterTermMapper;
-					var getter = function (filter, mapper) {
-						var term = filter.term;
-						var type = typeof term;
-						if (type != 'boolean' && type != 'number') {
-							if (!term) {
-								return undefined;
-							}
-						}
-						mapper = filter.filterTermMapper || mapper;
-						if (mapper) {
-							term = mapper(term);
-						}
-						return term;
-					};
-					if (col.filters.length == 1) {
-						$scope.filterModel[col.field] = getter(col.filters[0], mapper);
-					} else {
-						$scope.filterModel[col.field] = [];
-						for (var t = 0; t < col.filters.length; t++) {
-							$scope.filterModel[col.field].push(getter(col.filters[t], mapper))
-						}
-					}
-				}
-				getPage();
-			});
-		}
-	};
+	});
 
-	var getPage = function () {
-		$scope.isLoading = true;
-
-		var pageable = {
-			size: paginationOptions.pageSize,
-			page: (paginationOptions.pageNumber - 1),
-			sort: paginationOptions.sort
-		};
-
-		service.filter(pageable, $scope.filterModel, function (data) {
-			//console.log(data);
-			$scope.gridOptions.totalItems = data.totalElements;
-			$scope.gridOptions.data = data.content;
-			$scope.isLoading = false;
-		});
-	};
-
-	getPage();
+	$scope.loadPage();
 }])
 ;
