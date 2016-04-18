6c489af73182b098e0c494cf86dcfab9c8927a12
Valeriy.Vyrva <valery1707@gmail.com>
2016-04-18 16:06:22 +0600

    Поддержка списка полей для переноса в MySQL

--- src/main/resources/bro/amt_watch.sh
+++ src/main/resources/bro/amt_watch.sh
@@ -28,13 +28,16 @@ killall -v -9 tail
 
 #$1 - file_name
 #$2 - table_name
+#$3 - table_columns
 _watch() {
 	file_name=$1
 	table_name=$2
+	table_columns=$3
+	bro_columns=$(echo ${table_columns} | sed 's_`__g' | sed 's_,__g')
 	file_name_full="${log_dir}/${log_file_prefix}${file_name}"
 	touch ${file_name_full}
 	tail -f -n+0 ${file_name_full} \
-| /opt/bro/bin/bro-cut \
+| /opt/bro/bin/bro-cut ${bro_columns} \
 | sed -r 's_\t_", "_g' \
 | awk '{print "\"" $0 "\""}' \
 | sed 's_"-"_NULL_g' \
@@ -42,12 +45,12 @@ _watch() {
 | sed 's_"F"_FALSE_g' | sed 's_"T"_TRUE_g' \
 | sed -r 's_"(([[:digit:]]+)(\.[[:digit:]]+)?)"_\1_g' \
 | sed "s_\"_'_g" \
-| awk '{print "INSERT INTO target_table_name(ts, uid, `id.orig_h`, `id.orig_p`, `id.resp_h`, `id.resp_p`, proto, service, duration, orig_bytes, resp_bytes, conn_state, local_orig, local_resp, missed_bytes, history, orig_pkts, orig_ip_bytes, resp_pkts, resp_ip_bytes, tunnel_parents) \nVALUES (" $0 ");"}' \
+| awk '{print "INSERT INTO target_table_name(target_table_columns) \nVALUES (" $0 ");"}' \
 | sed "s/target_table_name/${table_name}/g" \
-| sed "s_, ',0,')_)_g" \
+| sed "s/target_table_columns/${table_columns}/g" \
 | mysql --user=${db_username} --password=${db_password} ${db_database} --batch &
 }
 
-_watch conn.log conn
+_watch conn.log conn 'ts, uid, `id.orig_h`, `id.orig_p`, `id.resp_h`, `id.resp_p`, proto, service, duration, orig_bytes, resp_bytes, conn_state, local_orig, local_resp, missed_bytes, history, orig_pkts, orig_ip_bytes, resp_pkts, resp_ip_bytes, tunnel_parents, amt_tasks_list'
 
 #ps -ef|grep 'tail'
