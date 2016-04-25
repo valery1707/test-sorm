10b640cec6de0b60c8ae5b94155fabb654a975b4
Valeriy.Vyrva <valery1707@gmail.com>
2016-04-25 15:33:31 +0600

    Преобразование размера ответа в таблице

---
+++ src/main/client/js/utils.js
@@ -0,0 +1,48 @@
+/**
+ * Format bytes to string
+ * @param bytes Bytes count
+ * @param decimals Count decimals after dot
+ * @param k Coefficient
+ * @param sizes Sizes
+ * @returns string
+ * @see https://stackoverflow.com/a/18650828
+ */
+function formatBytes(bytes, decimals, k, sizes) {
+	if (bytes == 0) {
+		return '0 Byte';
+	}
+	var dm = decimals + 0 || 2;
+	var i = Math.floor(Math.log(bytes) / Math.log(k));
+	return (bytes / Math.pow(k, i)).toFixed(dm) + ' ' + sizes[i];
+}
+/**
+ * Format bytes to string in 1024 mode
+ * @param bytes Bytes count
+ * @param decimals Count decimals after dot
+ * @returns string
+ * @see https://stackoverflow.com/a/18650828
+ */
+function formatBytes_1024(bytes, decimals) {
+	return formatBytes(bytes, decimals, 1024, ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']);
+}
+/**
+ * Format bytes to string in 1000 mode
+ * @param bytes Bytes count
+ * @param decimals Count decimals after dot
+ * @returns string
+ * @see https://stackoverflow.com/a/18650828
+ */
+function formatBytes_1000(bytes, decimals) {
+	return formatBytes(bytes, decimals, 1000, ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']);
+}
+
+angular.module('app').
+filter('formatBytes', function () {
+	return function (bytes, k, decimals) {
+		if (k == 1024) {
+			return formatBytes_1024(bytes, decimals);
+		} else {
+			return formatBytes_1000(bytes, decimals);
+		}
+	}
+});
