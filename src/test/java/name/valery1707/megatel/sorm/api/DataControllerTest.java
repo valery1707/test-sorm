d603a89370c4e253fe1501fb9bdbd958c231935f
Valeriy.Vyrva <valery1707@gmail.com>
2016-04-28 16:19:37 +0600

    Тесты: использование дополнительной конфигурации

--- src/test/java/name/valery1707/megatel/sorm/api/DataControllerTest.java
+++ src/test/java/name/valery1707/megatel/sorm/api/DataControllerTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
@@ -24,6 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Launcher.class)
 @WebAppConfiguration
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class DataControllerTest {
 	public static final MediaType CONTENT_TYPE = MediaType.APPLICATION_JSON;
 	public static final String ENCODING = "UTF-8";
