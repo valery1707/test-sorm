c0d208b6bab075b913f2bfa5934d8a16a824ce38
Valeriy.Vyrva <valery1707@gmail.com>
2016-04-28 17:00:42 +0600

    Более корректная работа с таймзонами

--- src/main/java/name/valery1707/megatel/sorm/DateUtils.java
+++ src/main/java/name/valery1707/megatel/sorm/DateUtils.java
@@ -7,8 +7,9 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 
 public class DateUtils {
-	private static final ZoneOffset DEFAULT_ZONE_OFFSET = ZoneId.systemDefault().getRules().getOffset(ZonedDateTime.now().toInstant());
-	private static final String DEFAULT_ZONE_OFFSET_NAME = DEFAULT_ZONE_OFFSET.toString();
+	public static final ZoneOffset DEFAULT_ZONE_OFFSET = ZoneId.systemDefault().getRules().getOffset(ZonedDateTime.now().toInstant());
+	public static final ZoneId DEFAULT_ZONE_ID_NORM = DEFAULT_ZONE_OFFSET.normalized();
+	public static final String DEFAULT_ZONE_OFFSET_NAME = DEFAULT_ZONE_OFFSET.toString();
 
 	public static ZonedDateTime bigDecimalToZonedDateTime(BigDecimal src) {
 		LocalDateTime localDateTime = LocalDateTime.ofEpochSecond(
@@ -16,7 +17,7 @@ public class DateUtils {
 				src.remainder(BigDecimal.ONE).movePointRight(src.scale()).abs().intValue(),
 				DEFAULT_ZONE_OFFSET
 		);
-		return ZonedDateTime.of(localDateTime, ZoneId.systemDefault());
+		return ZonedDateTime.of(localDateTime, DEFAULT_ZONE_ID_NORM);
 	}
 
 	public static BigDecimal zonedDateTime2BigDecimal(ZonedDateTime src) {
--- src/test/java/name/valery1707/megatel/sorm/DateUtilsTest.java
+++ src/test/java/name/valery1707/megatel/sorm/DateUtilsTest.java
@@ -3,24 +3,22 @@ package name.valery1707.megatel.sorm;
 import org.junit.Test;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class DateUtilsTest {
 	@Test
 	public void testBigDecimal_ZonedDateTime() throws Exception {
-		check("1461750541.915725", "2016-04-27T15:49:01.000915725");
+		check("1461750541.915725", "2016-04-27T09:49:01.000915725" + DateUtils.DEFAULT_ZONE_OFFSET_NAME);
 	}
 
 	private void check(String raw, String converted) {
 		BigDecimal bigDecimal = new BigDecimal(raw);
-		LocalDateTime localDateTime = LocalDateTime.parse(converted, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
-		ZonedDateTime zonedDateTime = ZonedDateTime.of(localDateTime, ZoneId.systemDefault());
-		assertEquals(String.format("raw(%s) -> converted(%s)", raw, converted), DateUtils.bigDecimalToZonedDateTime(bigDecimal), zonedDateTime);
-		assertEquals(String.format("converted(%s) -> raw(%s)", converted, raw), DateUtils.zonedDateTime2BigDecimal(zonedDateTime), bigDecimal);
+		ZonedDateTime zonedDateTime = ZonedDateTime.parse(converted, DateTimeFormatter.ISO_DATE_TIME)
+				.plusSeconds(DateUtils.DEFAULT_ZONE_OFFSET.getTotalSeconds());
+		assertEquals(String.format("raw(%s) -> converted(%s)", raw, converted), zonedDateTime, DateUtils.bigDecimalToZonedDateTime(bigDecimal));
+		assertEquals(String.format("converted(%s) -> raw(%s)", converted, raw), bigDecimal, DateUtils.zonedDateTime2BigDecimal(zonedDateTime));
 	}
 }
