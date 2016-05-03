22b55847d51136e34c499c0e1365f6392fe0ad47
Valeriy.Vyrva <valery1707@gmail.com>
2016-05-03 15:59:06 +0600

    MySQL: Сохранение пароля вместо передачи его в параметре

--- src/main/resources/bro/amt_watch.sh
+++ src/main/resources/bro/amt_watch.sh
@@ -32,10 +32,13 @@ log_file_prefix=$5
 _debug "Params: DbUser(${db_username}), DbPass(${db_password}), DbName(${db_database}), LogDir(${log_dir}), LogFilePrefix(${log_file_prefix})"
 
 _info "Warm up MySQL"
+echo "[client]
+password=${db_password}" > ~/.my.cnf
+chmod 600 ~/.my.cnf
 OUT=1
 while [ ${OUT} -ne 0 ] && [ ${db_warm_up_cnt} -gt 0 ]; do
 	let db_warm_up_cnt=db_warm_up_cnt-1
-	RES=$(echo "SELECT NOW();" | mysql --user=${db_username} --password=${db_password} ${db_database} 2>&1)
+	RES=$(echo "SELECT NOW();" | mysql --user=${db_username} ${db_database} 2>&1)
 	OUT=$?
 	_debug "MySQL warm up: ${RES}"
 	if [ ${OUT} -ne 0 ]; then
@@ -70,12 +73,13 @@ _watch() {
 | sed 's_"F"_FALSE_g' | sed 's_"T"_TRUE_g' \
 | sed -r 's_"(([[:digit:]]+)(\.[[:digit:]]+)?)"_\1_g' \
 | sed "s_\"_'_g" \
-| awk '{print "INSERT INTO target_table_name(target_table_columns) \nVALUES (" $0 ");"}' \
+| awk '{print "INSERT INTO target_table_name(target_table_columns) VALUES (" $0 ");"}' \
 | sed "s/target_table_name/${table_name}/g" \
 | sed "s/target_table_columns/${table_columns}/g" \
 | sed 's/_saved_double_quote/"_/g' \
 | sed "s/_saved_single_quote_/''/g" \
-| mysql --user=${db_username} --password=${db_password} ${db_database} --batch &
+| mysql --batch --force --reconnect --wait --unbuffered --user=${db_username} ${db_database} \
+&
 }
 
 _info "Run new log watchers"
