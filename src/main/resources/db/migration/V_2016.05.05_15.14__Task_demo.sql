d1ea607c9d777587e574458d9c5d2f6617b36469
Valeriy.Vyrva <valery1707@gmail.com>
2016-05-05 16:24:57 +0600

    Тестовое задание: запись в БД

---
+++ src/main/resources/db/migration/V_2016.05.05_15.14__Task_demo.sql
@@ -0,0 +1,2 @@
+INSERT INTO task (id, created_at, agency, client_alias, period_start, period_finish, note)
+VALUES (1, now(), 'MegaTel', 'Test', '2016-04-01 00:00:00', '2016-05-31 23:59:59', 'Note');
