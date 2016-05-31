646110345180ed45a7d4336b48b320a815583289
Valeriy.Vyrva <valery1707@gmail.com>
2016-05-31 18:29:25 +0600

    Подключение шаблонизатора к внутренним вьюхам

--- src/main/java/name/valery1707/megatel/sorm/ui/IndexController.java
+++ src/main/java/name/valery1707/megatel/sorm/ui/IndexController.java
@@ -1,14 +1,21 @@
 package name.valery1707.megatel.sorm.ui;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
+	@Value("${server.minification.enable}")
+	private boolean serveMinFiles = true;
+
 	@RequestMapping({"", "index.htm", "index.php"})
-	public String index() {
-		return "index.html";
+	public ModelAndView index() {
+		ModelAndView view = new ModelAndView("index");
+		view.addObject("minSuffix", serveMinFiles ? ".min" : "");
+		return view;
 	}
 }
