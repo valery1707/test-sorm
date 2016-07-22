3741984c139ed14a12e6b6366f393a6d108f456f
Valeriy.Vyrva <valery1707@gmail.com>
2016-07-22 18:33:17 +0600

    Оперативная БД (сервер): исправил создание ссылок на сервер

--- src/main/resources/db/migration/V_2016.07.20_16.33__Bro_createdBy.sql
+++ src/main/resources/db/migration/V_2016.07.20_16.33__Bro_createdBy.sql
@@ -1,4 +1,11 @@
-ALTER TABLE bro_conn ADD COLUMN created_by_id BIGINT COMMENT 'Сервер, на котором была создана запись' REFERENCES server(id);
-ALTER TABLE bro_files ADD COLUMN created_by_id BIGINT COMMENT 'Сервер, на котором была создана запись' REFERENCES server(id);
-ALTER TABLE bro_smtp ADD COLUMN created_by_id BIGINT COMMENT 'Сервер, на котором была создана запись' REFERENCES server(id);
-ALTER TABLE bro_http ADD COLUMN created_by_id BIGINT COMMENT 'Сервер, на котором была создана запись' REFERENCES server(id);
+ALTER TABLE bro_conn ADD COLUMN created_by_id BIGINT COMMENT 'Сервер, на котором была создана запись';
+ALTER TABLE bro_conn ADD CONSTRAINT fk$bro_conn$server$created_by FOREIGN KEY (created_by_id) REFERENCES server (id);
+
+ALTER TABLE bro_files ADD COLUMN created_by_id BIGINT COMMENT 'Сервер, на котором была создана запись';
+ALTER TABLE bro_files ADD CONSTRAINT fk$bro_files$server$created_by FOREIGN KEY (created_by_id) REFERENCES server (id);
+
+ALTER TABLE bro_smtp ADD COLUMN created_by_id BIGINT COMMENT 'Сервер, на котором была создана запись';
+ALTER TABLE bro_smtp ADD CONSTRAINT fk$bro_smtp$server$created_by FOREIGN KEY (created_by_id) REFERENCES server (id);
+
+ALTER TABLE bro_http ADD COLUMN created_by_id BIGINT COMMENT 'Сервер, на котором была создана запись';
+ALTER TABLE bro_http ADD CONSTRAINT fk$bro_http$server$created_by FOREIGN KEY (created_by_id) REFERENCES server (id);
